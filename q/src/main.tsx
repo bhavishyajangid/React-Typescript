8c155f413092c629bcacb75d83f9e6d638b5579f
bhavishyajangid <bhavishya8824@gmail.com>
2025-10-28 16:10:21 +0530

    fd

---
+++ q/src/main.tsx
@@ -0,0 +1,21 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import { ThemeProvider } from '@emotion/react'
+import {createTheme, CssBaseline} from '@mui/material'
+import App from './App'
+
+const theme = createTheme({
+  palette: {
+    mode: 'light',
+  },
+})
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+   <ThemeProvider theme={theme}>
+    <CssBaseline />
+    <App />
+   </ThemeProvider>
+  </StrictMode>,
+)
